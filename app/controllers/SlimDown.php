cfb277b4a43505d13c0c60d071d26e87e33f03be
Justin Heideman <justin@fiddlyio.com>
2015-03-10 12:16:14 -0500

    make calculate children and siblings correctly, display output, make slightly more OO

--- app/controllers/SlimDown.php
+++ app/controllers/SlimDown.php
@@ -6,42 +6,36 @@ use \Slim\Slim;
 class SlimdownBase {
 
     function __construct() {
+        
+        $this->flatStruct = array();
+        $markdownDir = APP_ROOT .'/pages';
+
         $this->app = Slim::getInstance();
         $this->parser =  new \Mni\FrontYAML\Parser();
+
+        //do actual file tree processing
+        $this->getFileStruct($markdownDir);
+        $this->processTreeInformation();
     }
 
-    function staticPage($slug=''){
+    function staticPage($slug=array()){
 
-        $flatStruct = $this->getFileStruct(APP_ROOT .'/pages');
+        $slug = "/".implode( "/", $slug); //TODO: Need to resolve dupe slashes, probably
 
-
-        $flatStruct = $this->processTreeInformation($flatStruct);
-
-        echo "<!-- \n";
-        var_dump($flatStruct);
-        echo "\n-->\n";
-
-
-        $mdFile = $this->getPage($slug);
-
-        if ($mdFile) {
-
-            $pageContents = file_get_contents($mdFile);
-
-            $parser = new \Mni\FrontYAML\Parser();
-            $document = $parser->parse($pageContents);
-            $yaml = $document->getYAML();
-            $html = $document->getContent();
-
-            $this->data = $yaml;
-            $this->data['html'] = $html;
-
-            $this->setTemplate();
-            $this->app->render($this->template, $this->data);
-        } else {
+        if (!array_key_exists($slug, $this->flatStruct)){
             $this->app->notFound();
             return;
         }
+
+        $this->data = (array) $this->flatStruct[$slug];   
+        $fileContents = file_get_contents($this->data['fullpath']);
+
+        $document = $this->parser->parse($fileContents);
+        $html = $document->getContent();
+        $this->data['html'] = $html;
+        $this->setTemplate();
+
+        $this->app->render($this->template, $this->data);
     }
 
     /**
@@ -71,8 +65,8 @@ class SlimdownBase {
      * Figure out what template to use based on the front matter
      */
     private function setTemplate(){
-        if (isset($this->data['template']) && !empty($this->data['template'])){
-            $template = APP_ROOT.'/app/templates/'.trim($this->data['template']).'.tpl';
+        if (isset($this->data->template) && !empty($this->data->template)){
+            $template = APP_ROOT.'/app/templates/'.trim($this->data->template).'.tpl';
             if (file_exists($template)){
                 $this->template = $template;
                 return;
@@ -118,10 +112,15 @@ class SlimdownBase {
         }
 
 
-        return $pages;
+        $this->flatStruct = $pages;
     }
 
-
+    /**
+     * Parse the front matter for each file and return an array
+     * TODO: return false on invalid or corrupt data
+     * @param  string $path absolute filepath to a markdown file to be parsed
+     * @return array       Front matter data
+     */
     private function getFileInfo($path){
         $pageContents = file_get_contents($path);
         $document = $this->parser->parse($pageContents);
@@ -130,11 +129,11 @@ class SlimdownBase {
     }
 
 
-    private function processTreeInformation($flatStruct){
+    private function processTreeInformation(){
 
-        //var_dump($flatStruct);
+        //$flatStruct = clone($this->flatStruct);
 
-        foreach ($flatStruct as $url => $info) {
+        foreach ($this->flatStruct as $url => $info) {
             $pathParts = explode( '/', trim($url,"/"));
             //var_dump($paths);
             array_pop($pathParts);
@@ -144,74 +143,52 @@ class SlimdownBase {
             $myDepth = substr_count($url, '/');
             $parentDepth = substr_count($parentPath, '/');
 
-            // echo "crrent: ".$url."\n";
-            // echo 'parent: '.$parentPath."\n";
-
-            // echo "crrent depth: ".$myDepth."\n";
-            // echo 'parent depth: '.$parentDepth."\n";
 
             //figure out the parent object
-            if (isset( $flatStruct[$parentPath] ) && $url !== '/' ){
-                $parent = clone($flatStruct[$parentPath]);
+            if (isset( $this->flatStruct[$parentPath] ) && $url !== '/' ){
+                $parent = clone($this->flatStruct[$parentPath]);
                 //make sure we don't end up with too much unneeded depth
                 unset($parent->parent, $parent->siblings, $parent->children);
-                $parent->path = $parentPath;
-                $flatStruct[$url]->parent = $parent;
+                $parent->url_path = $parentPath;
+                $this->flatStruct[$url]->parent = $parent;
             }
 
             $children = array();
             $siblings = array();
 
             //loop through the page struct and find sub-paths to calculate siblings and children
-            foreach($flatStruct as $structPath => $structInfo){
+            foreach($this->flatStruct as $potentialMatchPath => $potentialMatchInfo){
           
-    
-                $tempDepth = substr_count($structPath, '/');
-        
-                $potentialParentPath = substr($structPath, 0, strlen($parentPath));
+                $tempDepth = substr_count($potentialMatchPath, '/');  //current depth of potential match
 
-
-                // var_dump($potentialParentPath);
-                // var_dump($tempDepth);
-
+                //siblings
+                $potentialParentPath = substr($potentialMatchPath, 0, strlen($parentPath));
                 if ($potentialParentPath === $parentPath 
-                    && $tempDepth === $myDepth 
-                    //&& 
-                        ){
-                    $sib = clone($structInfo);
+                    && $tempDepth === $myDepth
+                    && $url !== $potentialMatchPath){
+                    $sib = clone($potentialMatchInfo);
                     unset($sib->parent, $sib->siblings, $sib->children);
-                    if ($structPath === $url){
-                        $sib->current = true;
-                    }
+                    $sib->url_path = $potentialMatchPath;
                     $siblings[] = $sib;
                 }
 
-
-                //figure out if the parent path matches the potential parent path and has the correct depth
-
-                // echo 'potential: '.$potentialParentPath."\n";
-                // echo "actual   : ".$parentPath."\n";
-                // echo 'depth    : '.$myDepth."\n";
-                // echo 'pdepth   : '.$parentDepth."\n";
-
-
-                /*
-                TODO: Need to figure out children
-                 */
-
+                //figure out children
+                $potentialChildrenPath = substr($potentialMatchPath, 0, strlen($url));
+                if ($potentialChildrenPath === $url 
+                    && ($myDepth+1) === $tempDepth){
+                    $child = clone($potentialMatchInfo);
+                    unset($child->parent, $child->siblings, $child->children);
+                    $child->url_path = $potentialMatchPath;
+                    $children[] = $child;
+                } 
             }
-
-
-            // if (!empty($children)){
-            //     $flatStruct[$url]->children = $children;
-            // }
+            if (!empty($children)){
+                $this->flatStruct[$url]->children = $children;
+            }
             if (!empty($siblings)){
-                $flatStruct[$url]->siblings = $siblings;
+                $this->flatStruct[$url]->siblings = $siblings;
             }
-
-
         }
-        return $flatStruct;
     }
 
 
--- app/templates/default.tpl
+++ app/templates/default.tpl
@@ -7,6 +7,32 @@
 
     </head>
     <body>
+
+        <nav>
+            {if isset($parent)}
+                 Parent: <a href="{$parent->url_path}">{$parent->title}</a> <br />
+            {/if}
+
+            {if isset($siblings)}
+                siblings:
+                 <ul>
+                {foreach from=$siblings item=sib}
+                    <li><a href="{$sib->url_path}">{$sib->title}</a></li>
+                {/foreach}
+                </ul>
+            {/if}
+
+            {if isset($children)}
+                children:
+                 <ul>
+                {foreach from=$children item=child}
+                    <li><a href="{$child->url_path}">{$child->title}</a></li>
+                {/foreach}
+                </ul>
+            {/if}
+
+        </nav>
+
         {block name='body'}
         {$html}
         {/block}
